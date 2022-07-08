c01ae1690e66db7f15ed7b7a9d1f2fdb2f2d9c7d
leoquiroa <leonelquiroa@gmail.com>
2022-07-08 18:20:22 +0000

    until chapter 23

---
+++ Intro/23.NumberOfOccurrences.cpp
@@ -0,0 +1,45 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+int BinarySearch (int A[], int n, int X, int OK) {
+    int Left=0, Right=n-1, mid;
+    int result=-1;
+    while (Left <= Right) {
+        mid = (Left+Right)/2;
+        if (X==A[mid]) {
+            result=mid;
+            if (OK==1) 
+                Right=mid-1; // First occurrence
+            if (OK==2) 
+                Left=mid+1; //Last occurrence
+        } 
+        else if (X<A[mid]){
+            Right=mid-1;
+        }
+        else{
+            Left=mid+1;
+        }
+    }
+    return result;
+}
+int main ()
+{
+    //total elements = 16
+    int A[] = {1,1,1,3,3,3,3,3,3,3,5,6,6,8,9,9};
+
+    /*
+    Array
+    total elements
+    find value
+    1 first occurrence and 2 last occurrence
+    */
+    int FirstOccurrence = BinarySearch (A, 16, 3, 1);
+    int LastOccurrence = BinarySearch (A, 16, 3, 2);
+
+    if (FirstOccurrence==-1) 
+        cout<<"The element is not in the array";
+    else 
+        cout<<"The element appears "<<LastOccurrence - FirstOccurrence +1;
+
+    return 0;
+}
