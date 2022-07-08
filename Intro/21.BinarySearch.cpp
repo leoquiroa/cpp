27e4aa96cfb87c7d53682aa3f569dc17ddf1273e
leoquiroa <leonelquiroa@gmail.com>
2022-07-08 17:41:28 +0000

    debug binary search

--- Intro/21.BinarySearch.cpp
+++ Intro/21.BinarySearch.cpp
@@ -1,28 +1,48 @@
 # include <bits/stdc++.h>
 using namespace std;
+#include <iostream>
+
 ifstream f("data.in");
 ofstream g("data.out");
-int A[1000], numberElements;
+
+int A[1000], numberElements, searchVal;
+
 int BinarySearch (int X) {
     int Left=1, Right=numberElements, mid;
 
     while (Left <= Right) {
         mid=(Left + Right) / 2;
 
-        if (X==A[mid]) return mid; // we have found X
-        else if (X<A[mid]) Right = mid-1; // we discard the second half of the array and the middle element
-                       else Left = mid+1; // we discard the first half of the array and the middle element
+        if (X==A[mid]){
+            cout << "Found it: " << "\n";
+            cout << mid;   // we have found X
+            cout << "\n";
+            return 1;
+        }
+        else if (X<A[mid]) 
+            Right = mid-1; // we discard the second half of the array and the middle element
+        else 
+            Left = mid+1; // we discard the first half of the array and the middle element
     }
     return -1;
 }
+
 int main ()
 {
-    f>>numberElements;
-    // array A is in ascending order
-    for (int i=1; i<=numberElements; ++i)
-        f>>A[i];
 
-    cout<<BinarySearch(21);
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> numberElements; // Get user input from the keyboard
+
+    // array A is in ascending order
+    cout << "Type all the new members in ascending order" << "\n";
+    for (int ix=1; ix<=numberElements; ++ix){
+        cout << "The " << ix << " member: ";
+        cin >> A[ix];
+    }
+        
+    cout << "Type the member to search: " << "\n";
+    cin >> searchVal;
+    BinarySearch(searchVal);
 
     return 0;
 }
